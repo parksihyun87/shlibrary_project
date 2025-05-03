abdc1c25da50922031a0a2372a8de37641518c5e
박시현 <solarhyun87@gmail.com>
2025-05-04 02:30:07 +0900

    backup

--- src/SHLibraryManager.java
+++ src/SHLibraryManager.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class SHLibraryManager {
     DBConnect connect = new DBConnect();
 //시현1
@@ -9,15 +7,16 @@ public class SHLibraryManager {
         while(true){
             boolean endFlag=false;
             MenuManager.initMenu();
-            int select=MenuManager.selectInitMenu();
+            int select=MenuManager.menuInput(MenuManager.LOGIN,MenuManager.EXIT);
             switch(select){
                 case MenuManager.LOGIN:
-                    login();
+                    this.MainMenuProcess();//변경
                     break;
                 case MenuManager.NEWMEMBER:
                     break;
                 case MenuManager.EXIT:
                     endFlag=true;
+                    connect.releaseDB(); //변경
                     break;
             }
             if(endFlag){
@@ -25,43 +24,21 @@ public class SHLibraryManager {
             }
         }
     }
-
-
-    public boolean login(){
-
-
-        return true;
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    public void LibraryProcess(){
+    //  메인 메뉴 실행
+    public void MainMenuProcess(){
         while(true){
             boolean endFlag=false;
-            MenuManager.libraryMenu();
-            int select=MenuManager.selectLibraryMenu();
+            MenuManager.mainMenu();
+            int select=MenuManager.menuInput(MenuManager.LIBRARY, MenuManager.LOGOUT);
             switch(select){
                 case MenuManager.LIBRARY:
+                    this.LibraryProcess();
                     break;
                 case MenuManager.MYINFO:
+                    this.MemberProcess();
                     break;
                 case MenuManager.ADMIN:
+                    this.AdminProcess();
                     break;
                 case MenuManager.LOGOUT:
                     endFlag=true;
@@ -71,20 +48,80 @@ public class SHLibraryManager {
             }
         }
     }
-
+    //  도서관 메뉴 실행
+    public void LibraryProcess(){
+        while(true) {
+            boolean endFlag = false;
+            LibraryManager.libraryMenu();
+            int select = MenuManager.menuInput(LibraryManager.BOOKSEARCH,LibraryManager.EXITLIBRARY);
+            switch (select) {
+                case LibraryManager.BOOKSEARCH:
+                    LibraryManager.BookSearchProcess();
+                    break;
+                case LibraryManager.BESTSELLER:
+                    LibraryManager.BestsellerProcess();
+                    break;
+                case LibraryManager.INTEREST:
+                    LibraryManager.InterestCategoryProcess();
+                    break;
+                case LibraryManager.BOOKRETURN:
+                    LibraryManager.BookReturnProcess();
+                    break;
+                case LibraryManager.BOOKREQUEST:
+                    LibraryManager.BookRequestProcess();
+                    break;
+                case LibraryManager.EXITLIBRARY:
+                    endFlag = true;
+                    break;
+            }
+            if (endFlag) {
+                break;
+            }
+        }
+    }
+    //  회원 메뉴 실행
+    public void MemberProcess(){
+        while(true) {
+            boolean endFlag = false;
+            MemberManager.memberMenu();
+            int select = MenuManager.menuInput(MemberManager.MYBOOK, MemberManager.EXITMEMBER);
+            switch (select) {
+                case MemberManager.MYBOOK:
+                    MemberManager.RentalStatusProcess();
+                    break;
+                case MemberManager.MYINFO:
+                    MemberManager.MyInfoProcess();
+                    break;
+                case MemberManager.DELAY:
+                    MemberManager.OverdueCheckProcess();
+                    break;
+                case MemberManager.EXITMEMBER:
+                    endFlag = true;
+                    break;
+            }
+            if (endFlag) {
+                break;
+            }
+        }
+    }
+    //  관리자 메뉴 실행
     public void AdminProcess(){
         while(true){
             boolean endFlag=false;
             AdminManager.AdminMenu();
-            int select=AdminManager.selectAdminMenu();
+            int select=MenuManager.menuInput(AdminManager.MEMBERADMIN, AdminManager.EXITADMIN);
             switch(select){
                 case AdminManager.MEMBERADMIN :
+                    AdminManager.MemberAdminProcess();
                     break;
                 case AdminManager.BOOKADMIN:
+                    AdminManager.BookAdminProcess();
                     break;
                 case AdminManager.GRADEADMIN:
+                    AdminManager.GradeAdminProcess();
                     break;
                 case AdminManager.BLACKADMIN:
+                    AdminManager.BlackAdminProcess();
                     break;
                 case AdminManager.EXITADMIN:
                     endFlag=true;
@@ -95,6 +132,4 @@ public class SHLibraryManager {
             }
         }
     }
-
-
 }
