2591fa694fc2d41b5d9cca2d980f1c469ad7374e
jangs-uxui <jangs.uxui@gmail.com>
2025-05-05 01:34:09 +0900

    업뎃

--- sql/shlibrary_usertbl.sql
+++ sql/shlibrary_usertbl.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 8.0.11, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
 --
 -- Host: localhost    Database: shlibrary
 -- ------------------------------------------------------
--- Server version	8.0.11
+-- Server version	8.0.42
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
- SET NAMES utf8 ;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -21,15 +21,15 @@
 
 DROP TABLE IF EXISTS `usertbl`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usertbl` (
   `userid` char(4) NOT NULL,
   `userpw` char(4) NOT NULL,
   `username` varchar(5) NOT NULL,
   `userdate` date DEFAULT NULL,
-  `userage` int(11) DEFAULT NULL,
+  `userage` int DEFAULT NULL,
   `userinterest` varchar(6) DEFAULT NULL,
-  `usergrade` varchar(6) DEFAULT NULL,
+  `usergrade` varchar(6) DEFAULT '신입회원',
   PRIMARY KEY (`userid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -40,9 +40,31 @@ CREATE TABLE `usertbl` (
 
 LOCK TABLES `usertbl` WRITE;
 /*!40000 ALTER TABLE `usertbl` DISABLE KEYS */;
-INSERT INTO `usertbl` VALUES ('aaaa','1111','김정태','2025-03-23',27,NULL,'신입회원'),('adm1','adm1','장희용','2015-07-23',NULL,NULL,'관리자'),('adm2','adm2','박시현','2015-06-12',NULL,NULL,'관리자'),('bbbb','2222','조주언','2022-07-12',NULL,'철학','우수회원'),('cccc','3333','김민규','2024-12-04',NULL,NULL,'신입회원'),('dddd','4444','박준홍','2023-01-22',21,'자연과학','우수회원'),('eeee','5555','최서영','2018-05-15',28,'기술과학','모범회원'),('ffff','6666','임현범','2024-02-27',30,'예술','일반회원'),('gggg','7777','장창현','2024-04-30',19,'문학','일반회원'),('hhhh','8888','지재호','2020-09-12',34,NULL,'장기연체자'),('iiii','9999','장선아','2023-11-05',28,NULL,'장기연체자'),('jjjj','0000','최세령','2024-10-02',NULL,'총류','신입회원');
+INSERT INTO `usertbl` VALUES ('aaaa','1111','김정태','2025-03-23',27,NULL,'신입회원'),('adm1','adm1','장희용','2015-07-23',NULL,NULL,'관리자'),('adm2','adm2','박시현','2015-06-12',NULL,NULL,'관리자'),('bbbb','2222','조주언','2022-07-12',NULL,'철학','우수회원'),('cccc','3333','김민규','2024-12-04',NULL,NULL,'신입회원'),('dddd','4444','박준홍','2023-01-22',21,'자연과학','우수회원'),('eeee','5555','최서영','2018-05-15',28,'기술과학','모범회원'),('ffff','6666','임현범','2024-02-27',30,'예술','일반회원'),('gggg','7777','장창현','2024-04-30',19,'문학','일반회원'),('hhhh','8888','지재호','2020-09-12',34,NULL,'장기연체자'),('iiii','9999','장선아','2023-11-05',28,NULL,'장기연체자'),('jjjj','0000','최세령','2024-10-02',NULL,'총류','신입회원'),('user','qwe','홍길동','2025-05-04',30,'철학','신입회원');
 /*!40000 ALTER TABLE `usertbl` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb3_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `insert_userdate` BEFORE INSERT ON `usertbl` FOR EACH ROW begin
+	if (new.userdate is null) then
+		set new.userdate = curdate();
+    end if;
+    if new.usergrade is null then
+		set new.usergrade = '신입회원';
+    end if;
+end */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -53,4 +75,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-05-01 17:40:19
+-- Dump completed on 2025-05-04 23:44:15
