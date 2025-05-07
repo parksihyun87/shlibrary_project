14143b55d154e1228d623d93a35cb94fc3fdb377
choi-seonah <dondo0412@naver.com>
2025-05-07 15:27:42 +0900

    my info check

--- src/MemberManager.java
+++ src/MemberManager.java
@@ -1,6 +1,4 @@
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class MemberManager {
     //* 멤버 변수 부
@@ -28,13 +26,15 @@ public class MemberManager {
     }
 
     // 나의 정보 메뉴 실행
-    public void myInfoProcess() {
+    public void myInfoProcess() throws SQLException {
+        //MemberManager_safe_name man_safe=new MemberManager_safe_name();
         while (true) {
             boolean endFlag = false;
             MenuManager.myinfo();
             int select = MenuManager.menuInput(MenuManager.CHECKINFO, MenuManager.EXITMYINFO);
             switch (select) {
                 case MenuManager.CHECKINFO:
+                    checkMyInfo();
                     break;
                 case MenuManager.UPDATEINFO:
                     break;
@@ -63,6 +63,35 @@ public class MemberManager {
             }
             if (endFlag) {
                 break;
+            }
+        }
+    }
+
+    public void checkMyInfo() throws SQLException {
+        DBConnect db=new DBConnect();
+        db.initDBConnect();
+        String checkquery="select * from usertbl where userid=?";
+
+        try(Connection conn=db.getConnection();
+            PreparedStatement pstmt=conn.prepareStatement(checkquery)){
+            pstmt.setString(1, currentUser.getUserid());
+            ResultSet rs=pstmt.executeQuery();
+
+            while(rs.next()){
+                String id=rs.getString("userid");
+                String pw=rs.getString("userpw");
+                String name=rs.getString("username");
+                Date userdate=rs.getDate("userdate");
+                int age=rs.getInt("userage");
+                String interest=rs.getString("userinterest");
+                String grade=rs.getString("usergrade");
+
+
+                System.out.println("===== "+name+"님의 정보"+ "=====");
+                System.out.println("ID : "+id+'\t' +"PW : "+pw);
+                System.out.println("가입일자 : "+userdate);
+                System.out.println("나이 : "+age+'\t'+"관심분야 : "+interest+'\t'+"등급 : "+grade);
+
             }
         }
     }
